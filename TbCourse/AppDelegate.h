d79e419a86bb9dcd3dd4e35d146e4242d8d33911
武仲 <wuzhong@taobao.com>
2014-07-26 00:53:46 +0800

    Initial Commit

---
+++ TbCourse/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TbCourse
+//
+//  Created by wuzhong on 14-7-26.
+//  Copyright (c) 2014年 com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
